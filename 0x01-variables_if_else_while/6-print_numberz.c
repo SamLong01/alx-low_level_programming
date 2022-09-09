bf59bce739a317ea1bc657ee0f24fedaf578313d
SamLong01 <samueladebayo1015@gmail.com>
2022-09-09 22:44:25 +0000

    final 3rd commit?

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints single digit numbers
+ * in base 10
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+int x;
+
+for (x = 0; x <= 9; x++)
+putchar(x);
+putchar("\n");
+return (0);
+}
