250202fe9e1ed8a9b33b946ad86c3058d34d0428
SamLong01 <samueladebayo1015@gmail.com>
2022-09-09 01:20:02 +0000

    5th commit edit?

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - print a given phrase
+ *using printf
+ *Return: 1 if successful
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
