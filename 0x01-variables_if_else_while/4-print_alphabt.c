5f6047fe6b6d421a2ff457467a35081d638ad86f
SamLong01 <samueladebayo1015@gmail.com>
2022-09-09 21:47:22 +0000

    3rd commit???

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * except e and q
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+char alpha = 'a';
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+if (alpha != 'e' && alpha != 'q')
+{
+putchar(alpha);
+putchar('\n');
+}
+}
+return (0);
+}
