ec5f8ce9c26c1fe497fe8b685d72146743d8f05b
SamLong01 <samueladebayo1015@gmail.com>
2022-09-14 00:23:41 +0000

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,13 @@ int main(void)
 {
 	_putchar('_');
 	_putchar('p');
-	_putchar(Ã'u');
+	_putchar('u');
 	_putchar('t');
-	_putchar(Ã'c');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 
 	return (0);
+}
