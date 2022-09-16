71c8e33d2a60cb274f46b9046e90973c143aa10f
SamLong01 <samueladebayo1015@gmail.com>
2022-09-16 07:00:17 +0000

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,9 +12,12 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
-			_putchar(35);
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar(35);
+			_putchar('\n');
+		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
