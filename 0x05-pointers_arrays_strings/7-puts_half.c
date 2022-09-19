c1dad988826ec68cab49ec5a128bf496e1b28a70
SamLong01 <samueladebayo1015@gmail.com>
2022-09-19 21:59:55 +0000

    task 8

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,12 +13,12 @@ void puts_half(char *s)
 	if (len % 2 == 0)
 	{
 		for (i = len / 2; i < len; i++)
-			_putschar(s[i]);
+			_putchar(s[i]);
 	}
 	else
 	{
 		for (i = len / 2 + 1; i < len; i++)
-			_putschar(s[i]);
+			_putchar(s[i]);
 	}
 	_putchar('\n');
 }
