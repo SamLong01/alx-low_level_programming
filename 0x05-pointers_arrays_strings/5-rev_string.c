cf8436a3fff7032b2e55fcc328877fa7e2bea51f
SamLong01 <samueladebayo1015@gmail.com>
2022-09-19 21:29:55 +0000

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int i = strlen(s) - 1;
+	int j = 0;
+
+	char tmp;
+
+	while (i > j)
+	{
+		tmp = s(i);
+		s(i) = s(j);
+		s(j) = tmp;
+		i--;
+		j++;
+	}
+}
