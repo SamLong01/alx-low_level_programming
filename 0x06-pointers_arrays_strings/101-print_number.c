52cbea44f2cfd637402554249f32c7ad143e739f
SamLong01 <samueladebayo1015@gmail.com>
2022-09-22 22:52:56 +0100

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,5 +7,16 @@
 
 void print_number(int n)
 {
-	_putchar(n);
+	unsigned int a;
+	a = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		a = -n;
+	}
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + '0');
 }
