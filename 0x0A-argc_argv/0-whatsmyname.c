5561181f2599bb21881b325b5d49eb7c7c50bfcd
SamLong01 <samueladebayo1015@gmail.com>
2022-09-30 22:11:59 +0100

    print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-        printf("argc = %d\n", argc);
-        printf("%s\n", argv[0]);
+        if (argc >= 1)
+		printf("%s\n", argv[0]);
         return (0);
 }
