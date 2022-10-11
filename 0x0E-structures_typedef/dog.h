0b0936d8dcc40d1b575c960e3aef44684448648b
SamLong01 <samueladebayo1015@gmail.com>
2022-10-11 11:40:48 +0100

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new data type containing name, age and owner
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
+
+#endif
