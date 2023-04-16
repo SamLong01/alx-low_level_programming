86636ec3ad22cd9bc00de10f6b1cd087041a27eb
SamLong01 <samueladebayo1015@gmail.com>
2023-04-16 22:19:09 +0100

    search

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for a value in a sorted array of integers
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: the value to search for
+ *
+ * Return: the index where value is located or
+ * if array is NULL, -1
+*/
---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers
+ *
+ * @list:  pointer to the head of the skip list to search in
+ * @value: value to search for
+ *
+ * Return: pointer on the first node where value is located or
+ * NULL, If value is not present in list or if head is NULL
+*/
